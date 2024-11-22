2d540fa62340423a53cfb21d6f40482490f90a15
Ethan Ball <uthbees@gmail.com>
2024-11-22 13:28:26 -0700

    Complete basic requirements

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -1,28 +1,44 @@
-use godot::classes::ISprite2D;
-use godot::classes::Sprite2D;
+use godot::classes::CharacterBody2D;
+use godot::classes::ICharacterBody2D;
 use godot::prelude::*;
 
+const GRAVITY: real = 1000.0;
+const WALK_SPEED: real = 1000.0;
+const JUMP_SPEED: real = 1000.0;
+
 #[derive(GodotClass)]
-#[class(base=Sprite2D)]
+#[class(base=CharacterBody2D)]
 struct Player {
-    speed: f64,
-    angular_speed: f64,
-    base: Base<Sprite2D>,
+    base: Base<CharacterBody2D>,
 }
 
 #[godot_api]
-impl ISprite2D for Player {
-    fn init(base: Base<Sprite2D>) -> Self {
-        Self {
-            speed: 400.0,
-            angular_speed: std::f64::consts::PI,
-            base,
-        }
+impl ICharacterBody2D for Player {
+    fn init(base: Base<CharacterBody2D>) -> Self {
+        Self { base }
     }
 
     fn process(&mut self, delta: f64) {
-        #[allow(clippy::cast_possible_truncation)]
-        let radians = (self.angular_speed * delta) as f32;
-        self.base_mut().rotate(radians);
+        // Update velocity
+        let mut velocity = self.base().get_velocity();
+        velocity.y += delta as real * GRAVITY;
+
+        let input = Input::singleton();
+        if input.is_action_pressed("ui_left") {
+            velocity.x = -WALK_SPEED;
+        } else if input.is_action_pressed("ui_right") {
+            velocity.x = WALK_SPEED;
+        } else {
+            velocity.x = 0.0;
+        }
+
+        if input.is_action_pressed("ui_up") && self.base().is_on_floor() {
+            velocity.y = -JUMP_SPEED;
+        }
+
+        self.base_mut().set_velocity(velocity);
+
+        // Update position
+        self.base_mut().move_and_slide();
     }
 }
