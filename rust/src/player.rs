ac38ad495927cd04cf5f731c38281fe9af59c601
Ethan Ball <uthbees@gmail.com>
2024-12-03 11:45:37 -0700

    Polish

--- rust/src/player.rs
+++ rust/src/player.rs
@@ -2,43 +2,94 @@ use godot::classes::CharacterBody2D;
 use godot::classes::ICharacterBody2D;
 use godot::prelude::*;
 
-const GRAVITY: real = 1000.0;
-const WALK_SPEED: real = 1000.0;
-const JUMP_SPEED: real = 1000.0;
-
 #[derive(GodotClass)]
 #[class(base=CharacterBody2D)]
 struct Player {
+    #[export]
+    gravity: real,
+    #[export]
+    max_horizontal_speed: real,
+    #[export]
+    jump_speed: real,
+    #[export]
+    ground_friction_factor: real,
+    #[export]
+    air_friction_factor: real,
+    #[export]
+    acceleration_factor: real,
+    #[export]
+    max_air_jumps: i32,
+    available_air_jumps: i32,
     base: Base<CharacterBody2D>,
 }
 
 #[godot_api]
 impl ICharacterBody2D for Player {
     fn init(base: Base<CharacterBody2D>) -> Self {
-        Self { base }
+        Self {
+            base,
+            gravity: 1000.0,
+            max_horizontal_speed: 100.0,
+            jump_speed: 1000.0,
+            ground_friction_factor: 0.2,
+            air_friction_factor: 0.1,
+            acceleration_factor: 0.025,
+            max_air_jumps: 1,
+            available_air_jumps: 0,
+        }
     }
 
     fn process(&mut self, delta: f64) {
-        // Update velocity
         let mut velocity = self.base().get_velocity();
-        velocity.y += delta as real * GRAVITY;
+        velocity.y += delta as real * self.gravity;
 
         let input = Input::singleton();
-        if input.is_action_pressed("ui_left") {
-            velocity.x = -WALK_SPEED;
-        } else if input.is_action_pressed("ui_right") {
-            velocity.x = WALK_SPEED;
-        } else {
+        let left_pressed = input.is_action_pressed("ui_left");
+        let right_pressed = input.is_action_pressed("ui_right");
+
+        let mut turning_around = false;
+
+        if left_pressed {
+            velocity.x -= self.max_horizontal_speed * self.acceleration_factor;
+            turning_around = velocity.x > 0.0;
+        } else if right_pressed {
+            velocity.x += self.max_horizontal_speed * self.acceleration_factor;
+            turning_around = velocity.x < 0.0;
+        }
+
+        if turning_around || (!left_pressed && !right_pressed) {
+            if self.base().is_on_floor() {
+                velocity.x *= 1.0 - self.ground_friction_factor;
+            } else {
+                velocity.x *= 1.0 - self.air_friction_factor;
+            }
+        }
+
+        if velocity.x < 5.0 && velocity.x > -5.0 {
             velocity.x = 0.0;
         }
-
-        if input.is_action_pressed("ui_up") && self.base().is_on_floor() {
-            velocity.y = -JUMP_SPEED;
+        if velocity.x > self.max_horizontal_speed {
+            velocity.x = self.max_horizontal_speed;
+        } else if velocity.x < -self.max_horizontal_speed {
+            velocity.x = -self.max_horizontal_speed;
         }
 
-        self.base_mut().set_velocity(velocity);
+        if self.base().is_on_floor() {
+            self.available_air_jumps = self.max_air_jumps;
+        }
 
-        // Update position
-        self.base_mut().move_and_slide();
+        if input.is_action_just_pressed("ui_jump") {
+            if self.base().is_on_floor() {
+                velocity.y = -self.jump_speed;
+            } else if self.available_air_jumps > 0 {
+                self.available_air_jumps -= 1;
+                velocity.y = -self.jump_speed;
+            }
+        }
+
+        // Update values
+        let mut base = self.base_mut();
+        base.set_velocity(velocity);
+        base.move_and_slide();
     }
 }
